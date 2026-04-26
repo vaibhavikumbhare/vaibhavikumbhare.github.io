6c05bcb8091cf46ca72f0cdc06691d80486dd46d
vaibhavikumbhare <vaibhavik2006@gmail.com>
2026-04-26 14:10:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
-// Typing Effect
-const roles = ["Vaibhavi 👋", "Web Developer 💻", "AI Enthusiast 🤖"];
-let i = 0, j = 0;
-let current = "";
-let deleting = false;
+const toggle = document.getElementById("darkToggle");
 
-function typeEffect() {
-  current = roles[i];
-
-  if (!deleting) {
-    j++;
-  } else {
-    j--;
-  }
-
-  document.getElementById("typing").textContent = current.substring(0, j);
-
-  if (!deleting && j === current.length) {
-    deleting = true;
-    setTimeout(typeEffect, 1000);
-  } else if (deleting && j === 0) {
-    deleting = false;
-    i = (i + 1) % roles.length;
-    setTimeout(typeEffect, 200);
-  } else {
-    setTimeout(typeEffect, deleting ? 50 : 100);
-  }
-}
-
-typeEffect();
-
-// AOS Animation
-AOS.init({
-  duration: 1000
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
 });
